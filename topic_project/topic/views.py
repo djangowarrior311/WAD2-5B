d5a9e76a7320add5aa82356e46ec36283d43d150
Jacob Liu <3070970l@student.gla.ac.uk>
2026-03-01 18:26:42 +0000

    fix directory structure to be less bad :/

---
+++ topic_project/topic/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.http import HttpRequest
+from typing import TypedDict
+
+
+class TestContext(TypedDict):
+    announcement: str
+
+
+# Create your views here.
+def index(request: HttpRequest):
+    context_dict: TestContext = {
+        "announcement": "Hello world!"
+    }
+
+    return render(request, "base.html", context=context_dict)
+
+
+# def test(request: HttpRequest):
+    
