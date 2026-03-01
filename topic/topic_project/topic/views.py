b6525957d64ca48ead854c51788500c3a3cc0ebd
Jacob Liu <3070970l@student.gla.ac.uk>
2026-03-01 14:18:01 +0000

    index page, hello world!

--- topic/topic_project/topic/views.py
+++ topic/topic_project/topic/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.http import HttpRequest
 
 # Create your views here.
+def index(request: HttpRequest):
+    return HttpResponse("Hello world!")
--- topic/topic_project/topic_project/urls.py
+++ topic/topic_project/topic_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from topic import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('admin/', admin.site.urls)
 ]
