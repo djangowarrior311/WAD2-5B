fb9ff7428c3f621306db079ae72f5f9b8f40655d
2975774s <2975774s@student.gla.ac.uk>
2026-03-06 14:00:51 +0000

    first iteration of register page

--- topic_project/topic_project/urls.py
+++ topic_project/topic_project/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from topic import views
 
 from django.urls import URLPattern, URLResolver
 
 urlpatterns: list[URLPattern | URLResolver] = [
     path('', views.index),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path("topic/", include("topic.urls")),
 ]
