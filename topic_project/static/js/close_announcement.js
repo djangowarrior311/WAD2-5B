dc0622a6f59d49ea2d775b055cb7348731218ef7
Jacob Liu <3070970l@student.gla.ac.uk>
2026-03-10 14:22:36 +0000

    announcement close button working

--- topic_project/static/js/close_announcement.js
+++ topic_project/static/js/close_announcement.js
@@ -1,5 +1,13 @@
 "use-strict"
-// announcement = document.getElementById("announcements")
-// announcement_close_button = document.getElementById("")
+let announcement = document.getElementById("announcements")
+// we only need to worry about one button 
+let closeButton = announcement.getElementsByTagName("button")[0]
+let defaultDisplay = announcement.style.display
 
-console.log("Hello world!")
+closeButton.addEventListener("click", () => {
+    if (announcement.style.display === "none") {
+        announcement.style.display = defaultDisplay;
+    } else {
+        announcement.style.display = "none";
+  }
+})
