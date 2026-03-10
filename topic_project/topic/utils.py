1a4a774fc81df074133c7eee91b894fc2f2f024b
2975774s <2975774s@student.gla.ac.uk>
2026-03-10 13:48:47 +0000

    removed annoying pylance error

--- topic_project/topic/utils.py
+++ topic_project/topic/utils.py
@@ -5,7 +5,9 @@ from .models import EmailVerification
 
 def send_verification_email(email: str) -> str:
     verification, created = EmailVerification.objects.update_or_create( email = email, defaults = {"code": EmailVerification.code})
-
+    # to remove the annoying pylance error
+    if created:
+        pass
     subject = "Your Verification Code for Topic"
     message = f"Your Verification code is: {verification.code}\nThis code expires in 10 minutes.\nThank you for signing up to Topic."
     from_email = settings.EMAIL or "noreply@localhost"
